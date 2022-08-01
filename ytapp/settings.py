9c4c3436d5356e58b771f99063e7aaa9e49fbc69
Abdullah Al Mahmud <abdulahalmahmud@gmail.com>
2022-08-01 07:47:48 +0600

    Add perodic task with celery

--- ytapp/settings.py
+++ ytapp/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from celery.schedules import crontab
+import ytapp.tasks
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,3 +128,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_BROKER_URL = "redis://redis:6379"
+CELERY_RESULT_BACKEND = "redis://redis:6379"
+CELERY_BEAT_SCHEDULE = {
+    "sample_task": {
+        "task": "ytapp.tasks.sample_task",
+        "schedule": crontab(minute="*/1"),
+    },
+}
