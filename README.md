4a54e3dd7a292ae14e7cf60f89a95b828fcfd6d1
Abdullah Al Mahmud <abdulahalmahmud@gmail.com>
2022-08-02 01:49:37 +0600

    Add a new command to reload celery

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-### Setup
+
+### Setup environment variables
+
+
+### Project setup
 docker-compose up -d --build
 docker-compose exec web python manage.py createsuperuser
 
--- ytvideo/management/commands/fetchstats.py
+++ ytvideo/management/commands/fetchstats.py
@@ -1,8 +1,12 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 class Command(BaseCommand):
     help = "Fetches youtube video stats for chanels defined"
 
     def handle(self, *args, **options):
-        self.stdout.write("Fetching video stats...") 
+        self.stdout.write("Fetching video statsS...")
+        self.stdout.write("Channels: %s" % (env("CHANNEL_LIST")))
