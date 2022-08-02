ac222ccb89b09c8590ae5b82a16d179bc2bd6918
Abdullah Al Mahmud <abdulahalmahmud@gmail.com>
2022-08-03 04:24:11 +0600

    Accept filter in the api

--- ytvideo/urls.py
+++ ytvideo/urls.py
@@ -1,9 +1,10 @@
+from posixpath import basename
 from django.urls import include, path
 from rest_framework import routers
 from .views import *
 
 router = routers.DefaultRouter()
-router.register(r'videos', YTVideoStatViewSet)
+router.register(r'videos', YTVideoStatList, basename="YTVideoStat")
 
 
 urlpatterns = [
--- ytvideo/views.py
+++ ytvideo/views.py
@@ -3,14 +3,27 @@ from django.template import loader
 from rest_framework import viewsets
 
 from .serializers import YTVideoSerializer
-from .models import YTVideoStat
+from .models import YTVideoStat, YTVideoTag
 
 def HomeView(request):
     template = loader.get_template('index.html')
     context = {}
     return HttpResponse(template.render(context, request))
 
-class YTVideoStatViewSet(viewsets.ModelViewSet):
-    queryset = YTVideoStat.objects.all()
+class YTVideoStatList(viewsets.ModelViewSet):
     serializer_class = YTVideoSerializer
-
+    
+    def get_queryset(self):
+        queryset = YTVideoStat.objects.all()
+        tag = self.request.query_params.get('tag')
+        if tag is not None:
+            matchedTags = YTVideoTag.objects.filter(tag__icontains=tag).values_list('videoId', flat=True)
+            print(matchedTags)
+        
+        sortByPerformance = self.request.query_params.get('sortByPerformance')
+        if sortByPerformance is not None:
+            order = "-videoPerformance"
+            if sortByPerformance == "asc":
+                order = "videoPerformance"
+            queryset = queryset.order_by(order)
+        return queryset
